eb06b3100962b9b8190f0b016fac2f21f2f8674d
ABDULMALIK ABDULKEHAR <abdulmalik.abdulkehar-ug@aau.edu.et>
2026-05-06 12:32:17 +0300

    Default scan CIDR and support auto interface selection

--- main.go
+++ main.go
@@ -10,21 +10,26 @@ import (
 
 func main() {
 
-
 	scanFlag := flag.Bool("scan", false, "Scan the network")
-	CIDR := flag.String("cidr", "", "CIDR for the network scan")
+	CIDR := flag.String("cidr", "", "CIDR for the network scan (defaults to interface subnet)")
 	cutFlag := flag.Bool("cut", false, "Cut off a device")
 	ipAddr := flag.String("ip", "", "IP address of the device to cut off (required if using cut option)")
 	mac := flag.String("mac", "", "MAC address of the device to cut off (required if using cut option)")
 	gateway := flag.String("g", "", "Gateway IP address")
-	ifaceName := flag.String("i", "wlp49s0", "Interface name")
+	ifaceName := flag.String("i", "auto", "Interface name (use 'auto' to detect)")
 	flag.Parse()
 
 	scanner := networkscan.NewNetworkScanner(*ifaceName)
 	defer scanner.Close()
 
 	if *scanFlag {
-		scanner.NetScan(*CIDR) 
+		if *CIDR == "" {
+			*CIDR = scanner.DefaultCIDR()
+		}
+		if *CIDR == "" {
+			log.Fatal("CIDR is required for scanning; pass -cidr or use an interface with an IPv4 subnet.")
+		}
+		scanner.NetScan(*CIDR)
 	}
 
 	if *cutFlag {
@@ -33,7 +38,7 @@ func main() {
 		}
 		var deviceToCut *networkscan.Device
 		if (*mac == "") {
-			devices := scanner.NetScan(*ipAddr + "/32") 
+			devices := scanner.NetScan(*ipAddr + "/32")
 
 			for _, device := range devices {
 				if device.IP.String() == *ipAddr {
@@ -41,7 +46,7 @@ func main() {
 					break
 				}
 			}
-		}else{
+		} else {
 			deviceToCut = &networkscan.Device{
 				IP:  net.ParseIP(*ipAddr),
 				MAC: net.HardwareAddr{},
@@ -54,9 +59,9 @@ func main() {
 
 		}
 		if deviceToCut != nil {
-			
+
 			log.Printf("Cut off device: IP: %s, MAC: %s, HOSTNAME: %s\n", deviceToCut.IP, deviceToCut.MAC, deviceToCut.HOSTNAME)
-			scanner.CutOffDevice(*deviceToCut,*gateway)
+			scanner.CutOffDevice(*deviceToCut, *gateway)
 		} else {
 			log.Printf("Device with IP: %s not found\n", *ipAddr)
 		}
